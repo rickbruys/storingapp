f0a6eb75cdb1fb7123a7e82c48f6fafdfb623038
rickbruijs <D283966@edu.rocwb.nl>
2021-03-09 13:46:07 +0100

    Update meldingenController.php

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -5,13 +5,20 @@ $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
-
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder) VALUES (:attractie, :capaciteit, :melder)";
 
 //3. Prepare
+$statment = $conn->prepare($query);
 
 //4. Execute
+$statment->execute([
+	":attractie" => $attractie,
+	":capaciteit" => $capaciteit,
+	":melder" => $melder
+]);
+
+header("Location: ../meldingen/index.php?msg=Melding opgeslagen");
